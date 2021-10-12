de53c93a47fc938481c1c2cc8caa3aa75bcacf23
stone-wall <virginiaprivacy@protonmail.com>
2021-10-11 22:00:58 -0400

    list all detected device vendor and product id's if no usable device is found.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.github.virginiaprivacycoalition"
-version = "1.0.6"
+version = "1.0.7"
 
 repositories {
     mavenCentral()
@@ -31,7 +31,7 @@ publishing {
         create<MavenPublication>("release") {
             group = "com.github.virginiaprivacycoalition"
             artifactId = "desktopusb"
-            version = "1.0.6"
+            version = "1.0.7"
             from(components["java"])
         }
     }

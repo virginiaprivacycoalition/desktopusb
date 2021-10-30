c166051b6ff93c1770c9a7743593812d34ab6e15
stone-wall <stonewall@pklite.xyz>
2021-10-30 08:10:36 -0400

    bump2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
-    kotlin("jvm") version "1.5.30"
+    kotlin("jvm") version "1.5.31"
     `java-library`
     java
     `maven-publish`
 }
 
 group = "com.github.virginiaprivacycoalition"
-version = "1.0.9"
+version = "1.1.0"
 
 repositories {
     mavenCentral()
@@ -18,7 +18,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     implementation("org.usb4java:usb4java:1.3.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2-native-mt")
-    implementation("com.github.virginiaprivacycoalition:sdr:0.2.5")
+    implementation("com.github.virginiaprivacycoalition:sdr:0.2.6")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
@@ -31,7 +31,7 @@ publishing {
         create<MavenPublication>("release") {
             group = "com.github.virginiaprivacycoalition"
             artifactId = "desktopusb"
-            version = "1.0.9"
+            version = "1.1.0"
             from(components["java"])
         }
     }
